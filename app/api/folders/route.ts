7368a9f603a1c5d56dcea32103058b5987d3a3af
Sathish Dath <sathishdutt0@gmail.com>
2025-03-21 00:13:18 +0530

    asd

--- app/api/folders/route.ts
+++ app/api/folders/route.ts
@@ -8,12 +8,12 @@ mongoose.connect(process.env.MONGODB_URI as string);
 export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query");
-  const data = await Folder.find();
+  // const data = await Folder.find();
   if (!query) {
     return NextResponse.json({ error: "Query is required" }, { status: 400 });
   }
 
-  const results = searchFolderStructure(query, data);
+  // const results = searchFolderStructure(query, data);
 
-  return NextResponse.json({ results });
+  return NextResponse.json({ results: query });
 };
